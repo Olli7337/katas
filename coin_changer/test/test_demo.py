ec9408d5c92631a5c444cac7a316b5f75aa59095
Olli Porvari <olliporvari@gmail.com>
2018-02-05 17:42:11 +0200

    Coins

--- coin_changer/test/test_demo.py
+++ coin_changer/test/test_demo.py
@@ -1,5 +1,39 @@
 import  unittest
 
+# 0 -> []
+# 10 -> [10]
+# 20 -> [20]
+# 30 -> [20, 10]
+# 40 -> [20, 20]
+
+def coin_changer(amount):
+    pass
+    coins = []
+    while amount >= 100:
+        coins.append(100)
+        amount=amount-100
+    while amount >= 50:
+        coins.append(50)
+        amount=amount-50
+    while amount >= 20:
+        coins.append(20)
+        amount=amount-20
+    while amount >= 10:
+        coins.append(10)
+        amount = amount - 10
+
+
+    return coins
+
+
 class MyTest(unittest.TestCase):
-    def test(self):
-        self.assertEqual(True, False)
+    def test_0(self):
+        self.assertEqual([], coin_changer(0))
+    def test_10(self):
+        self.assertEqual([10], coin_changer(10))
+        self.assertEqual([20], coin_changer(20))
+        self.assertEqual([50], coin_changer(50))
+    def test_30(self):
+        self.assertEquals([20,10],coin_changer(30))
+        self.assertEquals([20,20],coin_changer(40))
+        self.assertEquals([100],coin_changer(100))
